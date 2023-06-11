87e23aa78951412737688f3f83da0cd22ebf857f
mlaposata <mikelaposata@gmail.com>
2023-06-11 16:40:58 -0400

    updating to 1.20

--- src/main/java/com/dreamtea/enchant/Indestructible.java
+++ src/main/java/com/dreamtea/enchant/Indestructible.java
@@ -1,16 +1,20 @@
 package com.dreamtea.enchant;
 
+import com.dreamtea.tag.TagUtils;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.enchantment.UnbreakingEnchantment;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.AxeItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 import static com.dreamtea.IndestructibleEnchant.INDESTRUCTIBLE_ENCHANT;
 import static com.dreamtea.IndestructibleEnchant.NAMESPACE;
+import static com.dreamtea.tag.IndestructibleTag.INDESTRUCTIBLE_ITEMS;
 
 public class Indestructible extends Enchantment {
   protected Indestructible(Rarity weight) {
@@ -41,11 +45,22 @@ public class Indestructible extends Enchantment {
     return super.canAccept(other);
   }
 
+
+  @Override
+  public boolean isAcceptableItem(ItemStack stack) {
+    if (TagUtils.itemIsIn(stack, INDESTRUCTIBLE_ITEMS)) {
+      return false;
+    }
+    return super.isAcceptableItem(stack);
+  }
+
   public static boolean isIndestructible(ItemStack item){
-    return EnchantmentHelper.get(item).containsKey(INDESTRUCTIBLE_ENCHANT);
+    return EnchantmentHelper.get(item).containsKey(INDESTRUCTIBLE_ENCHANT)
+      || TagUtils.itemIsIn(item, INDESTRUCTIBLE_ITEMS);
   }
 
   public static Enchantment register() {
-    return Registry.register(Registry.ENCHANTMENT, new Identifier(NAMESPACE, "indestructible"), new Indestructible(Enchantment.Rarity.RARE));
+    return Registry.register(Registries.ENCHANTMENT, new Identifier(NAMESPACE, "indestructible"), new Indestructible(Enchantment.Rarity.RARE));
   }
+
 }
